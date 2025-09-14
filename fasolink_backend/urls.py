f1467bbda34d1b82530bb38a57e8c9cd24252ab2
aliseidu8855 <aliseidu8855@gmail.com>
2025-09-14 03:07:57 -0700

    Integrate Cloudinary for production media storage

--- fasolink_backend/urls.py
+++ fasolink_backend/urls.py
@@ -20,5 +20,5 @@ urlpatterns += [
 ]
 
 
-if settings.DEBUG or not settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG or not settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
