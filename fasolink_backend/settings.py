61cbfc66c7f94b9c3926e850ce8506017461deb6
aliseidu8855 <aliseidu8855@gmail.com>
2025-09-14 10:51:49 +0000

    FIX: Reconfigure Cloudinary using explicit documentation method

--- fasolink_backend/settings.py
+++ fasolink_backend/settings.py
@@ -201,3 +201,8 @@ LOCALE_PATHS = [
 
 # --- Cloudinary Media Storage Configuration ---
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET_KEY'),
+}
