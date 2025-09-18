e14bea6f072da64efbbeb2d555cbb68d47ec8548
aliseidu8855 <aliseidu8855@gmail.com>
2025-09-18 15:16:45 +0000

    "update asgi.py and Procfile"

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne -b 0.0.0.0 -p 8000 fasolink_backend.asgi:application
+web: daphne -b 0.0.0.0 -p $PORT fasolink_backend.asgi:application
 release: python manage.py collectstatic --noinput && python manage.py migrate
--- fasolink_backend/asgi.py
+++ fasolink_backend/asgi.py
@@ -9,20 +9,21 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+# IMPORTANT: Configure settings before importing Django/Channels components
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fasolink_backend.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from api.middleware import TokenAuthMiddlewareStack
 import api.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fasolink_backend.settings')
-
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-	"http": django_asgi_app,
-	"websocket": TokenAuthMiddlewareStack(
-		URLRouter(
-			api.routing.websocket_urlpatterns
-		)
-	),
+    "http": django_asgi_app,
+    "websocket": TokenAuthMiddlewareStack(
+        URLRouter(
+            api.routing.websocket_urlpatterns
+        )
+    ),
 })
